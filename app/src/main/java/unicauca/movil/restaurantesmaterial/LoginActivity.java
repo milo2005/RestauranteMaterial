b28ddb7637f3d8d1fc99a7fbf2423e15a4b5bbc9
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-11-24 20:08:13 -0500

    Login implemented

--- app/src/main/java/unicauca/movil/restaurantesmaterial/LoginActivity.java
+++ app/src/main/java/unicauca/movil/restaurantesmaterial/LoginActivity.java
@@ -3,11 +3,17 @@ package unicauca.movil.restaurantesmaterial;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import unicauca.movil.restaurantesmaterial.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        //binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+
     }
 }
